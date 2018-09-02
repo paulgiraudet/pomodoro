6e9d4b52423ded34d298ebe2cbfb784d673274dc
Tictac <paul.giraudet@gmail.com>
2018-09-02 15:17:59 +0200

    adding timer function with autoswitch between session and break time

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
 var breakCount = 5 ;
 var sessionCount = 25 ;
+var temporaryCount = 25 ;
+var switchOnOff = 1 ;
+var seconds = 1;
+var id ="";
 
 $(document).ready(function(){
+
+$("#timerType").text
+  function timerOn(count){
+    var typeOfTimer = $("#timerType").text();
+    seconds--;
+    if(seconds<10 && seconds>-1){
+      $("#timer").text(sessionCount+":0"+seconds);
+    }
+    else if(seconds===-1){
+      seconds=59;
+      sessionCount--;
+      if(sessionCount===-1){
+        sessionCount=breakCount;
+        breakCount=temporaryCount;
+        temporaryCount=sessionCount;
+        sessionCount--;
+        if(typeOfTimer==="Session"){
+          $("#timerType").text("Break");
+        }
+        else{
+          $("#timerType").text("Session");
+        }
+      }
+      $("#timer").text(sessionCount+":"+seconds);
+    }
+    else{
+      $("#timer").text(sessionCount+":"+seconds);
+    }
+  }
 
   $("#breakTimeMinus").click(function(){
     if(breakCount > 1){
@@ -20,13 +53,33 @@ $(document).ready(function(){
     if(sessionCount > 1){
       sessionCount--;
       $("#sessionLength").text(sessionCount);
+      $("#timer").text(sessionCount);
+      temporaryCount--;
     }
   });
   $("#sessionTimePlus").click(function(){
     if(sessionCount < 25){
       sessionCount++;
+      temporaryCount++;
       $("#sessionLength").text(sessionCount);
+      $("#timer").text(sessionCount);
     }
   });
+
+
+  $("#controlTimer").click(function(){
+    if(switchOnOff === 1){
+      switchOnOff=0;
+      $("#controlTimer").removeClass("fa-play").addClass("fa-pause");
+      timerOn();
+      id=setInterval(timerOn,1000);
+
+    }
+    else if(switchOnOff === 0){
+      switchOnOff=1;
+      $("#controlTimer").removeClass("fa-pause").addClass("fa-play");
+      clearInterval(id);
+    }
+  })
 
 });

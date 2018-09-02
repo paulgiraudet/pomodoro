5298c7798d81298713633b2fa618ad2f7b6999ee
Tictac <paul.giraudet@gmail.com>
2018-09-02 13:49:29 +0200

    adding counter from 1 to 25 for sessionLength

--- js/main.js
+++ js/main.js
@@ -6,14 +6,27 @@ $(document).ready(function(){
   $("#breakTimeMinus").click(function(){
     if(breakCount > 1){
       breakCount--;
-    $("#breakLength").text(breakCount);
-  }
+      $("#breakLength").text(breakCount);
+    }
   });
   $("#breakTimePlus").click(function(){
     if(breakCount < 5){
-    breakCount++;
-    $("#breakLength").text(breakCount);
-  }
-  })
+      breakCount++;
+      $("#breakLength").text(breakCount);
+    }
+  });
+
+  $("#sessionTimeMinus").click(function(){
+    if(sessionCount > 1){
+      sessionCount--;
+      $("#sessionLength").text(sessionCount);
+    }
+  });
+  $("#sessionTimePlus").click(function(){
+    if(sessionCount < 25){
+      sessionCount++;
+      $("#sessionLength").text(sessionCount);
+    }
+  });
 
 });
